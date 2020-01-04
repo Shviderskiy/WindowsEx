1f976b8912e8d056562c0ee58fc472ed41765a6b
Shviderskiy <shviderskiy2014@gmail.com>
2020-01-04 08:14:48 +0300

    refactoring

--- tests/EscapeCommandLineArgA.cpp
+++ tests/EscapeCommandLineArgA.cpp
@@ -7,7 +7,7 @@
 
 #include "WindowsEx.h"
 
-static std::string EscapeCommandLineArgA_mock(char const * string_)
+static std::string EscapeCommandLineArgA_wrapper(char const * string_)
 {
     std::unique_ptr<char[], decltype(&LocalFree)> result(
                 EscapeCommandLineArgA(string_), LocalFree);
@@ -18,27 +18,27 @@ BOOST_AUTO_TEST_SUITE(EscapeCommandLineArgA_test)
 
 BOOST_AUTO_TEST_CASE(empty)
 {
-    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_mock(""), R"("")");
+    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_wrapper(""), R"("")");
 }
 
 BOOST_AUTO_TEST_CASE(space)
 {
-    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_mock(" "), R"(" ")");
+    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_wrapper(" "), R"(" ")");
 }
 
 BOOST_AUTO_TEST_CASE(backslash)
 {
-    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_mock(R"(\)"), R"(\)");
+    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_wrapper(R"(\)"), R"(\)");
 }
 
 BOOST_AUTO_TEST_CASE(quote)
 {
-    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_mock(R"(")"), R"("\"")");
+    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_wrapper(R"(")"), R"("\"")");
 }
 
 BOOST_AUTO_TEST_CASE(space_and_quotes)
 {
-    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_mock(R"(" ")"), R"("\" \"")");
+    BOOST_CHECK_EQUAL(EscapeCommandLineArgA_wrapper(R"(" ")"), R"("\" \"")");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- tests/GetErrorLabelA.cpp
+++ tests/GetErrorLabelA.cpp
@@ -6,7 +6,7 @@
 
 #include "WindowsEx.h"
 
-static std::string GetErrorLabelA_mock(DWORD error_code_)
+static std::string GetErrorLabelA_wrapper(DWORD error_code_)
 {
     char const * result = GetErrorLabelA(error_code_);
     return result == nullptr ? "" : result;
@@ -16,18 +16,18 @@ BOOST_AUTO_TEST_SUITE(GetErrorLabelA_test)
 
 BOOST_AUTO_TEST_CASE(ERROR_SUCCESS_)
 {
-    BOOST_CHECK_EQUAL(GetErrorLabelA_mock(ERROR_SUCCESS), "ERROR_SUCCESS");
+    BOOST_CHECK_EQUAL(GetErrorLabelA_wrapper(ERROR_SUCCESS), "ERROR_SUCCESS");
 }
 
 BOOST_AUTO_TEST_CASE(ERROR_INVALID_DLL_)
 {
-    BOOST_CHECK_EQUAL(GetErrorLabelA_mock(ERROR_INVALID_DLL),
+    BOOST_CHECK_EQUAL(GetErrorLabelA_wrapper(ERROR_INVALID_DLL),
                       "ERROR_INVALID_DLL");
 }
 
 BOOST_AUTO_TEST_CASE(_0xDEAFBEEF)
 {
-    BOOST_CHECK_EQUAL(GetErrorLabelA_mock(0xDEAFBEEF), "");
+    BOOST_CHECK_EQUAL(GetErrorLabelA_wrapper(0xDEAFBEEF), "");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
